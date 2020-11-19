fa60031ef48900af06613e78d30a83d7c03348e7
Praneeth-22 <yennampraneeth@gmail.com>
2020-11-19 15:49:45 +0530

    Updates

---
+++ precache-manifest.0fa28edac6104ce220dc70b1232e77eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a14409527d53aa60db8b3dd33576ef93",
+    "url": "/WhatsappClone/index.html"
+  },
+  {
+    "revision": "1e0a81c3de7750aa21b0",
+    "url": "/WhatsappClone/static/css/main.a602bbdf.chunk.css"
+  },
+  {
+    "revision": "f89494ba4c88b419f7ca",
+    "url": "/WhatsappClone/static/js/2.9ec9ec2c.chunk.js"
+  },
+  {
+    "revision": "9cf9d893b5c51de0aa6dd9e07be64537",
+    "url": "/WhatsappClone/static/js/2.9ec9ec2c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e0a81c3de7750aa21b0",
+    "url": "/WhatsappClone/static/js/main.ddb17830.chunk.js"
+  },
+  {
+    "revision": "af711d4783eb894e5942",
+    "url": "/WhatsappClone/static/js/runtime-main.def6ada4.js"
+  }
+]);
